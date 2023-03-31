be2180340d1bfd0da5ed1bf5529ade69de9c2495
Damien MATHIEU <damien.mathieu612@gmail.com>
2023-03-31 16:12:20 +0200

    Setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./App.scss";
+
+function App() {
+  return (
+    <div>
+      <p className="pink">Hello World!</p>
+      <h1 className="pink">Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
