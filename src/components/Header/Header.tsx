1644b3bdc882c26b70067869dff54d9c14917618
Damien MATHIEU <damien.mathieu612@gmail.com>
2023-04-01 17:59:48 +0200

    WIP

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import styles from './internal/header.module.scss'
+import { Hexagon } from 'react-feather'
+
+function Header() {
+  const [showMenu, setShowMenu] = useState(false)
+
+  function toggleMenu() {
+    setShowMenu(!showMenu)
+  }
+
+  return (
+    <nav className={styles.container}>
+      <div className={styles.logoContainer}>
+        <Hexagon />
+      </div>
+      <div className={styles.linksContainer}>
+        <a href="#" className={styles.linkContainer}>
+          <p>Présentation</p>
+        </a>
+        <a href="#" className={styles.linkContainer}>
+          <p>Les jeux</p>
+        </a>
+        <a href="#" className={styles.linkContainer}>
+          <p>Equipes</p>
+        </a>
+        <a href="#" className={styles.linkContainer}>
+          <p>S inscrire</p>
+        </a>
+      </div>
+    </nav>
+  )
+}
+
+export default Header
--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,21 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import "./styles/base/reset.scss";
-import "./index.scss";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import './styles/base/reset.scss'
+import './index.scss'
+import Homepage from './routes/Homepage/Homepage'
 
-const root = ReactDOM.createRoot(
-  document.getElementById("root") as HTMLElement
-);
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Homepage />
+  }
+])
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
-);
+)
