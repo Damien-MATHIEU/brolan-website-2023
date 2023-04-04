7ca160619427b4444a6d5fe8a88452a69f5facb0
Damien MATHIEU <damien.mathieu612@gmail.com>
2023-04-04 13:06:27 +0200

    Neon blincking done

---
+++ src/components/TextNeon/TextNeon.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import ITextNeon from './internal/textNeon.interface'
+import styles from './internal/textNeon.module.scss'
+
+function TextNeon({
+  children,
+  color = 'pink',
+  blinkingArrayFast = null,
+  blinkingArraySlow = null,
+  size = 'h1'
+}: ITextNeon) {
+  const splitedText = children.split('')
+
+  console.log(splitedText)
+  const styledText = splitedText.map((letter, index) => {
+    return (
+      <span
+        key={index}
+        className={`${styles.letter} ${
+          blinkingArrayFast?.includes(index) ? styles.letterBlinkFast : undefined
+        } ${blinkingArraySlow?.includes(index) ? styles.letterBlinkSlow : undefined}`}>
+        {letter}
+      </span>
+    )
+  })
+
+  switch (size) {
+    case 'h1':
+      return (
+        <h1
+          className={`${styles.word} ${
+            color === 'aquamarine' ? styles.wordAquamarine : styles.wordPink
+          }`}>
+          {styledText}
+        </h1>
+      )
+      break
+
+    case 'h2':
+      return (
+        <h2
+          className={`${styles.word} ${
+            color === 'aquamarine' ? styles.wordAquamarine : styles.wordPink
+          }`}>
+          {styledText}
+        </h2>
+      )
+      break
+
+    case 'h3':
+      return (
+        <h3
+          className={`${styles.word} ${
+            color === 'aquamarine' ? styles.wordAquamarine : styles.wordPink
+          }`}>
+          {styledText}
+        </h3>
+      )
+      break
+
+    default:
+      return (
+        <h1
+          className={`${styles.word} ${
+            color === 'aquamarine' ? styles.wordAquamarine : styles.wordPink
+          }`}>
+          {styledText}
+        </h1>
+      )
+      break
+  }
+}
+
+export default TextNeon
--- src/routes/Homepage/Homepage.tsx
+++ src/routes/Homepage/Homepage.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../../components/Header/Header'
+import HeroSection from '../../components/HeroSection/HeroSection'
 
 function Homepage() {
   return (
     <>
       <Header />
+      <HeroSection />
     </>
   )
 }
