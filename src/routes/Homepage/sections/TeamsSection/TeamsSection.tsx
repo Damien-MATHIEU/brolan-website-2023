1a19c60ea1a16ca3f5eefad9e82ede2d4cbfd6e1
Damien MATHIEU <damien.mathieu612@gmail.com>
2023-04-06 16:39:08 +0200

    TeamsSection styling

--- src/routes/Homepage/sections/TeamsSection/TeamsSection.tsx
+++ src/routes/Homepage/sections/TeamsSection/TeamsSection.tsx
@@ -2,11 +2,12 @@ import React from 'react'
 import styles from './internal/teamsSection.module.scss'
 import TableNeon from '../../../../components/TableNeon/TableNeon'
 import TextNeon from '../../../../components/TextNeon/TextNeon'
+import ButtonOutline from '../../../../components/Buttons/ButtonOutline/ButtonOutline'
 
 function TeamsSection() {
   return (
     <div className={styles.container}>
-      <TextNeon blinkingArrayFast={[1]} blinkingArraySlow={[4]}>
+      <TextNeon blinkingArrayFast={[1]} blinkingArraySlow={[4]} propsClassName={styles.subTitle}>
         EQUIPES
       </TextNeon>
       <TableNeon
@@ -20,6 +21,7 @@ function TeamsSection() {
         ]}
         tableHeaders={["Nom d'équipe"]}
       />
+      <ButtonOutline center={true}>S&apos;inscrire</ButtonOutline>
     </div>
   )
 }
