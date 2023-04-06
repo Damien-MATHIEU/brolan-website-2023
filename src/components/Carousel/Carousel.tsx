30e391ee196fbe9202caf9a283593d963f71e71d
Damien MATHIEU <damien.mathieu612@gmail.com>
2023-04-06 13:35:47 +0200

    Carousel + CarouselSection

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,8 +1,9 @@
-import React, { useRef, useState } from 'react'
+import React from 'react'
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { Navigation, Pagination } from 'swiper'
+import { Autoplay, Pagination, Scrollbar } from 'swiper'
 import 'swiper/css'
-import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import 'swiper/css/scrollbar'
 import styles from './internal/carousel.module.scss'
 import FallGuys from './../../assets/Games/FallGuys.png'
 import Fortnite from './../../assets/Games/Fortnite.png'
@@ -14,13 +15,74 @@ import TrackMania from './../../assets/Games/TrackMania.png'
 function Carousel() {
   return (
     <div>
-      <Swiper navigation={true} modules={[Navigation, Pagination]} className={styles.mySwiper}>
-        <SwiperSlide>{FallGuys}</SwiperSlide>
-        <SwiperSlide>{Fortnite}</SwiperSlide>
-        <SwiperSlide>{GeoGuesser}</SwiperSlide>
-        <SwiperSlide>{Minecraft}</SwiperSlide>
-        <SwiperSlide>{Osu}</SwiperSlide>
-        <SwiperSlide>{TrackMania}</SwiperSlide>
+      <Swiper
+        loop={true}
+        autoplay={{
+          delay: 2500,
+          disableOnInteraction: false
+        }}
+        grabCursor={true}
+        pagination={{
+          clickable: true
+        }}
+        scrollbar={{
+          hide: true
+        }}
+        modules={[Autoplay, Pagination, Scrollbar]}
+        className={styles.mySwiper}>
+        <SwiperSlide>
+          <div className={styles.carouselSlideContainer}>
+            <img src={TrackMania} className={styles.carouselImage} />
+            <div className={styles.carouselTextContainer}>
+              <p className={styles.carouselText}>TrackMania</p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className={styles.carouselSlideContainer}>
+            <img src={Minecraft} className={styles.carouselImage} />
+            <div className={styles.carouselTextContainer}>
+              <p className={styles.carouselText}>Minecraft</p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className={styles.carouselSlideContainer}>
+            <img src={Fortnite} className={styles.carouselImage} />
+            <div className={styles.carouselTextContainer}>
+              <p className={styles.carouselText}>Fortnite</p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className={styles.carouselSlideContainer}>
+            <img src={GeoGuesser} className={styles.carouselImage} />
+            <div className={styles.carouselTextContainer}>
+              <p className={styles.carouselText}>GeoGuesser</p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className={styles.carouselSlideContainer}>
+            <img src={FallGuys} className={styles.carouselImage} />
+            <div className={styles.carouselTextContainer}>
+              <p className={styles.carouselText}>Fall Guys</p>
+            </div>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className={styles.carouselSlideContainer}>
+            <img src={Osu} className={styles.carouselImage} />
+            <div className={styles.carouselTextContainer}>
+              <p className={styles.carouselText}>Osu</p>
+            </div>
+          </div>
+        </SwiperSlide>
       </Swiper>
     </div>
   )
--- src/routes/Homepage/sections/CarouselSection/CarouselSection.tsx
+++ src/routes/Homepage/sections/CarouselSection/CarouselSection.tsx
@@ -1,11 +1,17 @@
 import React from 'react'
 import styles from './internal/carouselSection.module.scss'
 import Carousel from '../../../../components/Carousel/Carousel'
+import ButtonOutline from '../../../../components/Buttons/ButtonOutline/ButtonOutline'
+import TextNeon from '../../../../components/TextNeon/TextNeon'
 
 function CarouselSection() {
   return (
     <div className={styles.container}>
+      <TextNeon propsClassName={styles.subTitle}>JEUX</TextNeon>
       <Carousel />
+      <ButtonOutline center={true} fontSize="medium">
+        Tous les jeux
+      </ButtonOutline>
     </div>
   )
 }
