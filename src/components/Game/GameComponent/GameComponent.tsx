a20842941156de4fdf7012eea5af80e95e0dda35
Damien MATHIEU <damien.mathieu612@gmail.com>
2023-04-28 19:47:10 +0200

    Game page done

--- src/components/Game/GameComponent/GameComponent.tsx
+++ src/components/Game/GameComponent/GameComponent.tsx
@@ -10,11 +10,17 @@ import FortniteRules from './../../../data/rules/fortnite.json'
 import GeoGuessrRules from './../../../data/rules/geoguessr.json'
 import OsuRules from './../../../data/rules/osu.json'
 import MinecraftRules from './../../../data/rules/minecraft.json'
+import GameHomepage from './../../../assets/Tutorial/Minecraft/GameHomepage.jpg'
+import Tlauncher from './../../../assets/Tutorial/Minecraft/Tlauncher.jpg'
+import WebsiteHomepage from './../../../assets/Tutorial/Minecraft/WebsiteHomepage.jpg'
+import AddServer from './../../../assets/Tutorial/Minecraft/AddServer.jpg'
+import EnterIpAddress from './../../../assets/Tutorial/Minecraft/EnterIpAddress.jpg'
+import TrackmaniaImage from './../../../assets/Tutorial/Trackmania/image.jpg'
 
 function GameComponent() {
   const { gameName } = useParams()
 
-  console.log(gameName)
+  // console.log(gameName)
 
   if (gameName == undefined) {
     return null
@@ -42,6 +48,7 @@ function GameComponent() {
       break
 
     default:
+      return null
       break
   }
 
@@ -51,12 +58,53 @@ function GameComponent() {
         {gameName}
       </TextNeon>
       <TableNeon tableContent={tableRule} noHeader={true} />
-      {/* <h2 className={styles.subTitle}>
-        Les règles de chaque jeux arriveront une fois les inscriptions terminées
-      </h2>
-      <ButtonOutline center={true} propsOnClick="/">
-        Accueil
-      </ButtonOutline> */}
+      {gameName === 'minecraft' && (
+        <div className={styles.additionnalInfos}>
+          <p className={styles.additionnalInfo}>
+            Hello et bienvenu dans ce superbe tutoriel à but non lucratif, qui te permettra de jouer
+            sur le serveur en totale gratuité et légalité ! (oupsi pas sûr de cette partie là). Dans
+            un premier temps tu te rends sur ton navigateur préféré (genre Opera, faut oublier
+            explorer c&apos;est vraiment plus à la mode), et tu télécharge Tlauncher.
+          </p>
+          <img src={WebsiteHomepage} className={styles.image} />
+          <p className={styles.additionnalInfo}>
+            Ensuite tu arrives sur le superbe screen suivant :{' '}
+          </p>
+          <img src={Tlauncher} className={styles.image} />
+          <p className={styles.additionnalInfo}>
+            Sur la partie du bas, deux éléments sont importants : Le pseudo (normalement c&apos;est
+            toi qui le choisi, fin c&apos;est mieux), et le truc à droite (pose pas de question), tu
+            sélectionne « Optifine 1.18.2 » et tu cliques sur le gros bouton Jaune sur la droite
+            (Jaune ou orange ? vrai débat, laisse un comm).
+          </p>
+          <p className={styles.additionnalInfo}>
+            Ensuite, tu cliques sur multiplayer (c&apos;est mieux pour jouer à plusieurs)
+          </p>
+          <img src={GameHomepage} className={styles.image} />
+          <p className={styles.additionnalInfo}>
+            Puis sur « Add server » pour ajouter un serveur (c&apos;est la traduction littérale en
+            anglais en fait).
+          </p>
+          <img src={AddServer} className={styles.image} />
+          <p className={styles.additionnalInfo}>
+            Une fois que t&apos;en est là, t&apos;as fait à peu près 5%, t&apos;y es presque !{' '}
+          </p>
+          <p className={styles.additionnalInfo}>
+            Dans Srver address tu rentres : « 62.210.41.50:27323 » et tu fais Done.
+          </p>
+          <img src={EnterIpAddress} className={styles.image} />
+          <p className={styles.additionnalInfo}>
+            Voilà le serv est maintenant dans ta liste, tu peux cliquer droit sur les différents
+            niveaux qui te sont proposés (spoiler alert, le jour J ils seront fait dans
+            l&apos;ordre), bon training 😉
+          </p>
+        </div>
+      )}
+      {gameName === 'trackmania' && (
+        <div className={styles.additionnalInfos}>
+          <img src={TrackmaniaImage} className={styles.image} />
+        </div>
+      )}
     </div>
   )
 }
